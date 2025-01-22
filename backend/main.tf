9e1317d78134d686be05f71c247996349675d282
Thomas VIAUD <thomas.viaud@laddition.com>
2025-01-22 15:12:15 +0100

    refacto

--- backend/main.tf
+++ backend/main.tf
@@ -48,7 +48,6 @@ module "psql" {
   admin_password      = var.db_admin_password
   sku_name            = "Standard_B2ms"
   storage_mb          = 32768
-  version             = "13" # Spécifiez ici la version
   delegated_subnet_id = module.network.subnet_ids[0]
   backup_retention_days = 7
   ha_mode             = "ZoneRedundant"
