e26d0701a323be054ffee19f7908406a91cba4d0
Thomas VIAUD <thomas.viaud@laddition.com>
2025-01-23 16:00:30 +0100

    refacto

--- backend/staging/main.tf
+++ backend/staging/main.tf
@@ -6,6 +6,7 @@ provider "azurerm" {
   skip_provider_registration = true
 }
 
+# Module Network
 module "network" {
   source = "../../modules/network"
 
@@ -14,40 +15,40 @@ module "network" {
   location         = var.location
   route_table_name = var.route_table_name
   project_name     = var.project_name
+  resource_group_name = var.resource_group_name # Passer le nom correct du RG ici
   instances_number = var.instances_number
 }
 
+# Module DNS Zone
 module "dns_zone" {
   source = "../../modules/private_dns_zone"
-  #version = "1.0.0"
 
   location = var.location
-  resource_group_name = var.resource_group_name
+  resource_group_name = var.resource_group_name # Passer le nom correct du RG ici
 }
 
-
+# Module PostgreSQL
 module "psql" {
   source = "../../modules/psql"
-  #version = "1.0.0"
 
   location            = var.location
   azure_pg_name       = var.azure_pg_name
   azure_pgsubnet_name = var.azure_pgsubnet_name
   azure_pgvnet_name   = var.azure_pgvnet_name
   project_name        = var.project_name
-  rg_name             = module.network.rg_name
+  rg_name             = var.resource_group_name # Passer le nom correct du RG ici
   pg_hostname         = var.pg_hostname
-  postgresql_server_admin_login = var.postgresql_server_admin_login
+  postgresql_server_admin_login    = var.postgresql_server_admin_login
   postgresql_server_admin_password = var.postgresql_server_admin_password
-  postgresql_databases_names = var.databases_names
-  postgresql_databases_user = var.databases_user
-  postgresql_databases_password = var.postgresql_databases_password
+  postgresql_databases_names       = var.databases_names
+  postgresql_databases_user        = var.databases_user
+  postgresql_databases_password    = var.postgresql_databases_password
   postgresql_server_pgbouncer_enabled = var.pgbouncer_enabled
-  postgresql_server_configurations = var.postgresql_server_configurations
+  postgresql_server_configurations    = var.postgresql_server_configurations
 }
 
+# Resource Linux Virtual Machine
 resource "azurerm_linux_virtual_machine" "example" {
-
   count = var.instances_number
 
   name                = "${var.project_name}-${count.index}"
@@ -62,7 +63,6 @@ resource "azurerm_linux_virtual_machine" "example" {
   admin_ssh_key {
     username   = "adminuser"
     public_key = file("~/.ssh/id_rsa.pub")
-    
   }
 
   os_disk {
@@ -89,82 +89,21 @@ resource "azurerm_linux_virtual_machine" "example" {
   }
 }
 
-# resource "azurerm_linux_virtual_machine" "imported_vm" {
-
-#   count = var.instances_number
-
-#   name                = "${var.project_name}-${count.index}"
-#   resource_group_name = module.network.rg_name
-#   location            = var.location
-#   size                = "Standard_F2"
-#   admin_username      = "adminuser"
-#   network_interface_ids = [
-#     element(module.network.net_int_id, count.index),
-#   ]
-
-#   admin_ssh_key {
-#     username   = "adminuser"
-#     public_key = file("~/.ssh/id_rsa.pub")
-    
-#   }
-
-#   os_disk {
-#     caching              = "ReadWrite"
-#     storage_account_type = "Standard_LRS"
-#   }
-
-#   source_image_reference {
-#     publisher = "Canonical"
-#     offer     = "0001-com-ubuntu-server-jammy"
-#     sku       = "22_04-lts"
-#     version   = "latest"
-#   }
-
-#   provisioner "remote-exec" {
-#     inline = ["sudo apt install -y nginx && sudo service nginx start"]
-
-#     connection {
-#       type        = "ssh"
-#       host        = element(module.network.ip_address, count.index)
-#       user        = "adminuser"
-#       private_key = file("~/.ssh/id_rsa")
-#     }
-#   }
-# }
-
-module  "storage_account" {
+# Module Storage Account
+module "storage_account" {
   source = "../../modules/storage_account"
 
-  
- storage_account_name = var.storage_account_name
- resource_group_name = var.resource_group_name
- location = var.location
-  
+  storage_account_name = var.storage_account_name
+  resource_group_name  = var.resource_group_name # Passer le nom correct du RG ici
+  location             = var.location
 }
 
+# Module Log Analytics Workspace
 module "log_analytics_workspace" {
   source = "../../modules/log_analytics_workspace"
 
   location = var.location
-  resource_group_name = var.resource_group_name
+  resource_group_name = var.resource_group_name # Passer le nom correct du RG ici
   log_analytics_name = var.log_analytics_name
 }
 
-# module "container_registry" {
-#   source = "../../modules/container_registry"
-
-#   location = var.location
-#   resource_group_name = var.resource_group_name
-#   container_registry_name = var.container_registry_name
-#   container_registry_sku = var.container_registry_sku
-#   container_registry_admin_enabled = var.container_registry_admin_enabled
-#   container_registry_georeplication_locations = var.container_registry_georeplication_locations
-# container_registry_privatelink_name = var.container_registry_privatelink_name
-# container_registry_privatelink_subnet_id = var.container_registry_privatelink_subnet_id
-#   container_registry_regional_endpoint_enabled = var.container_registry_regional_endpoint_enabled
-#   container_registry_zone_redundancy_enabled = var.container_registry_zone_redundancy_enabled
-#   public_network_access_enabled = var.public_network_access_enabled
-#   container_registry_private_dns_zone_ids = var.container_registry_private_dns_zone_ids
-#   tags = var.tags
-
-# }
--- backend/staging/staging.tfvars
+++ backend/staging/staging.tfvars
@@ -1,127 +1,27 @@
 tenantId             = "b7b023b8-7c32-4c02-92a6-c8cdaa1d189c"
 subscriptionId       = "0696952d-ddcb-4855-a2de-0068909dbc91"
 location             = "francecentral"
-project_name         = "aa06mdo-back"
+project_name         = "toto-staging"
 resource_group_name  = "toto-staging"
 
-## Route table
-
-route_table_name = "m2dospv-rt"
-
-## VNET
-
-vnet_name = "example-vnet"
-vnet_address_space = ["10.0.0.0/16"]
-subnet_name = "example-subnet"
-storage_account_name = "m2dototo"
-
-## Postgresql
-
+## PostgreSQL
 postgresql_server_admin_login    = "psqladmin"
 postgresql_server_admin_password = "H@Sh1CoR3!"
-
-pgbouncer_enabled = true
-databases_names    = ["aa06_m2dospv", "aa06_m1dospv"]
-databases_user     = "m2dospv"
-#databases_password = "acdybc9bzJxWert8C"
-#server_zone        = "1"
+databases_names                  = ["toto_db_1", "toto_db_2"]
+databases_user                   = "toto_user"
+pgbouncer_enabled                = true
 postgresql_server_configurations = {
-  "azure.extensions" = {
-    value = "PGAUDIT,PLV8,PG_STAT_STATEMENTS"
-  },
   shared_preload_libraries = {
-    value = "pg_cron,pg_stat_statements,pgaudit"
-  },
-  log_line_prefix = {
-    value = "%m back=%b [%p] start=%s  %q user=%u@%r %a db=%d tag=%i "
+    value = "pg_stat_statements,pgaudit"
   },
   max_connections = {
     value = "1000"
-  },
-  max_wal_size = {
-    value = "1024"
-  },
-  "pg_qs.query_capture_mode" = {
-    value = "ALL"
-  },
-  "pgbouncer.default_pool_size" = {
-    value = "500"
-  },
-  "pgbouncer.ignore_startup_parameters" = {
-    value = "extra_float_digits"
-  },
-  "pgbouncer.max_client_conn" = {
-    value = "1000"
-  },
-  "pgbouncer.stats_users" = {
-    value = "adminuser"
-  },
-  "pgms_wait_sampling.query_capture_mode" = {
-    value = "ALL"
-  },
-  track_io_timing = {
-    value = "ON"
-  },
-  log_lock_waits = {
-    value = "ON"
-  },
-  log_statement = {
-    value = "all"
-  },
-  log_min_duration_statement = {
-    value = "1000"
-  },
-  log_statement_stats = {
-    value = "ON"
-  },
-  max_parallel_maintenance_workers = {
-    value = "64"
-  },
-  "metrics.autovacuum_diagnostics" = {
-    value = "on"
-  },
-  "metrics.collector_database_activity" = {
-    value = "ON"
-  },
-  "metrics.pgbouncer_diagnostics" = {
-    value = "ON"
-  },
-  "pg_stat_statements.track" = {
-    value = "TOP"
-  },
-  "pgaudit.log" = {
-    value = "DDL,READ,ROLE,WRITE"
-  },
-  "pgaudit.log_relation" = {
-    value = "ON"
-  },
-  "pgaudit.log_statement_once" = {
-    value = "ON"
-  },
-  "pgaudit.role" = {
-    value = "adminuser"
   }
 }
-  
-### Log analytics workspace
 
-log_analytics_name = "m2dos-log"
+## Network
+route_table_name = "toto-staging-rt"
+storage_account_name = "toto-staging-sa"
 
-
-
-# ## Container registry
-# container_registry_privatelink_name = "m2dosnetpve001"
-
-# container_registry_name                     = "m2dospvsvcctnr01"
-# container_registry_sku                      = "Premium"
-# container_registry_admin_enabled            = "true"
-# container_registry_georeplication_locations = "West Europe"
-# container_registry_zone_redundancy_enabled   = false
-# container_registry_regional_endpoint_enabled = true
-# container_registry_network_rule_set = [{
-#   default_action = "Deny"
-#   ip_rule         = [{
-#     action   = "Allow"
-#     ip_range = "4.1.10.20/32"
-#   }]
-# }]
+## Log Analytics
+log_analytics_name = "toto-staging-log"
